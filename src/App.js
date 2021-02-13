fca08aa730972b5643d0dae24312d1d892c70e12
Variiiest <varishgautamvari@gmail.com>
2021-02-14 01:22:23 +0530

    SAAS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React from "react";
+import BlogPosts from "./components/blog/BlogPosts";
+import Footer from "./components/Footer";
 // import {ClassComponent, ClassComponent2} from './components/ClassComponent';
 // import  Counter from './components/Counter';
 // import Click from './components/Click';
 // import ParentComp from './components/ParentComp';
 
 import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import Services from "./components/Services";
-import LatestPost from "./components/LatestPost";
-import LatestEvents from "./components/LatestEvents";
-import Footer from "./components/Footer";
 
 const App = ( ) => {
   return (
     <>
-    <Navbar/>
-    <Hero/>
-    <Services/>
-    <LatestPost/>
-    <LatestEvents/>
-    <Footer/>
+  <Navbar/>
+ <BlogPosts/>
+  <Footer/>
+  
     </>
   );
 };
---
+++ src/components/blog/TopBlogger.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class TopBlogger extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default TopBlogger
