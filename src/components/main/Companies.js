e2e1fc9c2b317c0f76c91f3c2ecd05e4796480dd
Variiiest <varishgautamvari@gmail.com>
2021-02-21 16:24:09 +0530

    SAAS

--- src/components/main/Companies.js
+++ src/components/main/Companies.js
@@ -4,7 +4,7 @@ export class Companies extends Component {
     render() {
         return (
             <div>
-                <section className="bg-white pt-7 pb-14">
+                <section className="bg-pink-200 pt-7 pb-14">
         <div className="container px-8 mx-auto sm:px-12 lg:px-20">
           <h1 className="text-sm font-bold tracking-wide text-center text-gray-800 uppercase mb-7">Trusted by top-leading companies.</h1>
           <div className="flex grid items-center justify-center grid-cols-4 grid-cols-12 gap-y-8">
